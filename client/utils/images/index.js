b3313096974eb5414ab95b08466c63a9e2dba7b4
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-07-20 20:21:47 -0600

    Update index.js

--- client/utils/images/index.js
+++ client/utils/images/index.js
@@ -1,4 +1,4 @@
 exports.PlainLogo = 'https://toxmod.xyz/images/ToxModLogo.png'
 exports.TransLogo = 'https://toxmod.xyz/images/ToxModTrans.png'
 exports.Banner = 'https://toxmod.xyz/images/ToxModBanner.png'
-exports.Animated = 'https://toxmod.xyz/images/ToxModLogo.gif'
+exports.Animated = 'https://toxmod.xyz/images/ToxModTrans.png'
