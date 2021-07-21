eb58d703ef8f20d863a0fd96c53934b25b17e648
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-07-20 18:58:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const mongo = require("mongoose");
 
 const Website = require('./website/server');
 const Client = require('./client/index');
+const config = require('@Settings/config');
 
 (async () => {
     
-    let client = await Client.init();
+    let client = await Client.init(config.token);
 
     console.log('[Tox Mod | Bot] Connected to the Discord API');
 
