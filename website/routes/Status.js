1e5f6c456db60223b138e0ef3c429d4f7d4e3cb2
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-07-21 09:21:03 -0600

    Update Status.js

--- website/routes/Status.js
+++ website/routes/Status.js
@@ -1,4 +1,6 @@
 const route = require("express").Router();
+const { checkAuth } = require('@Authorization/checkAuth');
+const { checkMaintenance } = require('@Authorization/checkMaintenance');
 const { renderPage } = require('@Templates/renderPage');
 
 route.get("/", checkMaintenance, async (req, res) => {
