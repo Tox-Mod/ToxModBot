61727d8f54fd96f2d1308b9f0d84273f055df20e
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-18 13:27:11 -0700

    Update warn.js

--- client/cmds/warn.js
+++ client/cmds/warn.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const CASES = require('@Database/cases');
 const SERVERS = require('@Database/servers');
 const CLOCK = require('@Database/clock');
@@ -96,7 +96,7 @@ module.exports.run = async (client, message, args, params) => {
      }) 
     } catch (err) {
 
-        let ErrorEmbed = new Discord.MessageEmbed()
+        let ErrorEmbed = new MessageEmbed()
          .setTitle('Internal Error | Hmmm')
          .setColor(Colors.Error)
          .setDescription('Something went wrong here, Please try again or Contact my Dev Team.')
