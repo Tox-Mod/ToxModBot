321d7c193686f6d800b06c47c76367641da350c4
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-17 21:57:30 -0700

    Add files via upload

--- website/server.js
+++ website/server.js
@@ -1,5 +1,3 @@
-require("module-alias/register");
-
 const express = require("express");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
@@ -16,47 +14,31 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const { Client, Collection } = require("discord.js");
 
-const client = new Client({
-  disableEveryone: true,
-  disabledEvents: ["TYPING_START"],
-  restTimeOffset: 0,
-});
+const getFilesSync = require("./fileWalk");
 
-client.commands = new Collection();
-client.aliases = new Collection();
+class App {
+  constructor(client, locals = {}) {    
+    this.express = express();
+    
+    this.express.set("client", client);
+    this.express.set('views', 'views');
+    this.express.set("view engine", "ejs");
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: true }));
+    this.express.use(express.static(__dirname + "/public"));
+    this.express.use('/images', express.static(__dirname + "/images"));
+    this.express.use(passport.initialize());
+    this.express.use(passport.session());
+    this.express.use(cookieParser());
+    this.express.locals = locals;
+    this
+      .loadRoutes()
+      .loadErrorHandler();
 
-const commands = require('@Structure/commands');
-commands.run(client);
+    passport.serializeUser((user, done) => done(null, user));
+    passport.deserializeUser((obj, done) => done(null, obj));
 
-// Then Discord
-client.login(settings.token).catch(console.error);;
-
-// Discord Client Ready
-client.on("ready", async () => {
-  console.log(`[Tox Mod | Web] Successfully connected to the Discord API! as ${client.user.username}`);
-});
-
-app.disable("server");
-
-// Middleware
-app.set("client", client);
-app.set('views', 'views');
-app.set("view engine", "ejs");
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(__dirname + "/public"));
-app.use('/images', express.static(__dirname + "/images"));
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(cookieParser());
-
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((obj, done) => done(null, obj));
-
-passport.use(new Strategy({
+    passport.use(new Strategy({
       clientID: settings.clientID,
       clientSecret: settings.clientSecret,
       callbackURL: settings.domain,
@@ -66,37 +48,51 @@ passport.use(new Strategy({
       // eslint-disable-line no-unused-vars
       // On login we pass in profile with no logic.
       process.nextTick(() => done(null, profile));
-}));
+    }));
+  }
 
-/**
- * MEMORY STORE
- */
- app.use(
-  session({
-    cookie: { maxAge: require("ms")("10 years") },
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-    secret: "gGvp2tCfyrmk6sQeMrMGGdSw3aSfdmhfNzzJytYtTDEvHjqNLQsuynsA7WpxmEcgWvBaZr3rj9kXmM8uq4LtFXucCRAJMjXRvw7sW4fUhDradQNnseMe59vY2UBm6vRB2bbDTvdsx9XEPp4y2J4wVStRm4wF95Xk6HZeNwYMDYVuPdvZWh25bpmf3Hqa6v4jyj3XnA5AKCgGFExyevtGG35Bs563eMy74DA8D9FVbVdbWYvrgP5z25Y5Bg8tn5CW",
-    resave: false,
-    saveUninitialized: false
-  })
-);
+  listen(port) {
+    return new Promise((resolve) => this.express.listen(port, resolve));
+  }
 
-// Main Router
-app.use("/", router);
+  
+  loadRoutes() {
+    const routesPath = path.join(__dirname, "./routes");
+    const routes = getFilesSync(routesPath);
 
-app.listen(settings.port, async () => {
+    if (!routes.length) return this;
 
-  console.log(`[Tox Mod | Web] Listening to the Web Server on Port: ${settings.port}`)
-    await mongoose
-      .connect(settings.mongo_url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        autoIndex: false,
-        poolSize: 5,
-        connectTimeoutMS: 10000,
-        family: 4,
-      })
-      .then(() =>
-        console.log("[Tox Mod | Web] Successfully connected to the Database!")
-      );
-});
+    routes.forEach((filename) => {
+      const route = require(path.join(routesPath, filename));
+      
+      const routePath = filename === "Router.js" ? "/" : `/${filename.slice(0, -3)}`;
+
+      try {
+        this.express.use(routePath, route);
+      } catch (error) {
+        console.error(`Error occured with the route "${filename}"\n\n${error}`);
+      }
+    });
+
+    return this;
+  }
+
+  
+  loadErrorHandler() {
+    this.express.use((error, _req, res, _next) => {
+      const { message, statusCode = 500 } = error;
+      if (statusCode >= 500) console.error(error);
+
+      res.status(statusCode).send({
+        message,
+        status: statusCode
+      });
+    });
+
+    return this;
+  }
+}
+
+
+
+module.exports = App;
