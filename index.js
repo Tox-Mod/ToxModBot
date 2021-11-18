9ee79c15c966d7e89cd7099a344cdb6f5ae6d5c4
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-17 21:58:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ const Website = require('./website/server');
         useFindAndModify: false
     });
 
-    console.log(colors.yellow(`[Tox Mod - Bot] Connected to the database on `) + colors.underline.green(config.mongodb_url));
+    console.log(colors.yellow(`[Tox Mod - Logs] Connected to the database on `) + colors.underline.green(config.mongodb_url));
     
     let client = await Bot.init(config.token);
 
-    console.log(colors.yellow(`[Tox Mod - Bot] Logged in as `) + colors.underline.green(client.user.tag));
+    console.log(colors.yellow(`[Tox Mod - Logs] Logged in as `) + colors.underline.green(client.user.tag));
 
     await new Website(client).listen(process.env.PORT);
 
-    console.log(colors.yellow(`[Tox Mod - Bot] Running on port `) + colors.underline.green(process.env.PORT));
+    console.log(colors.yellow(`[Tox Mod - Logs] Running on port `) + colors.underline.green(process.env.PORT));
 })()
