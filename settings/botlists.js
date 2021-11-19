dee993251b0a1733086245b6bf4bc8267644b83f
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-18 22:45:26 -0700

    Update botlists.js

--- settings/botlists.js
+++ settings/botlists.js
@@ -1,4 +1,4 @@
 /** INFINITY BOT LIST AUTH AND BOT ID */
-exports.ClientID = '631558023109804032'
-exports.IBL_AUTH = 'tLA4s5fnX6RlLQwl6FgRvw1YHR7CGuTGwEhbNkH2VR3YLP9ZFa5MP4jMuL0BDU7Z3rUlH770QOgRVgNwfpuSJRWwsBfbdY47iQrE'
-exports.Paradise_AUTH = "jzktprseb51852szfzkxcp16922nhhiaabro"
+exports.ClientID = process.env.CLIENT_ID
+exports.IBL_AUTH = process.env.IBL_AUTH
+exports.Paradise_AUTH = process.env.PARADISE_AUTH
