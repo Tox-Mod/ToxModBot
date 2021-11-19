af892d46e3020bdbe0c36e89805233d22de1eed0
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-18 21:53:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ require("module-alias/register");
 const mongo = require("mongoose");
 const config = require("@Settings/config");
 const colors = require('colors');
-// const Bot = require('@Client/index');
+const Bot = require('@Client/index');
+const Shards = require('@Client/shards');
 const Website = require('./website/server');
 
 
@@ -18,12 +19,13 @@ const Website = require('./website/server');
 
     console.log(colors.yellow(`[Tox Mod - Logs] Connected to the database on `) + colors.underline.green(config.mongodb_url));
     
-    // let client = await Bot.init(config.token);
-    let client = require('@Client/shards.js');
+    let client = await Bot.init(config.token);
 
     console.log(colors.yellow(`[Tox Mod - Logs] Logged in as `) + colors.underline.green(client.user.tag));
 
     await new Website(client).listen(process.env.PORT);
 
     console.log(colors.yellow(`[Tox Mod - Logs] Running on port `) + colors.underline.green(process.env.PORT));
+
+    await Shards.init(client);
 })()
