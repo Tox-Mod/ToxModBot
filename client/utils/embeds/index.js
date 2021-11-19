71625151b8d6e6870109110fa9746f095062234e
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-19 09:29:12 -0700

    Update index.js

--- client/utils/embeds/index.js
+++ client/utils/embeds/index.js
@@ -1,3 +1,2 @@
-const version = require('../../../package.json')
-
-exports.Footer = `© 2021 - 2022 Tox Mod | v${version}`
+exports.Version = '2.1.1'
+exports.Footer = `© 2021 - 2022 Tox Mod | v2.1.1`
