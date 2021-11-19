b89ef6b2ee8d6617ca18719bd95e467e7a4163fe
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-19 15:59:07 -0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -30,7 +30,29 @@ exports.ErrLogs = process.env.ERR_LOGS
 * DEFINE THE USERS AND THEIR ACCESS 
 */
 
-exports.owners = process.env.OWNERS
-exports.admins = process.env.ADMINS
-exports.beta = process.env.BETA_WL
-exports.devs = process.env.DEVS
+exports.owners = [
+    "510065483693817867",
+    "486659270876856362",
+    "324646179134636043",
+    "713632188863610911",
+    "391376464064282627",
+    "673937399801184306"
+]
+
+exports.admins = [
+    "391376464064282627",
+    "673937399801184306"
+]
+
+exports.beta = [
+    "391376464064282627",
+    "673937399801184306",
+    "713632188863610911",
+    "324646179134636043"
+]
+
+exports.devs = [
+    "486659270876856362",
+    "713632188863610911",
+    "324646179134636043"
+]
