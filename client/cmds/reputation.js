72d95b0ea48f63f313a8d38d5f27ec919a8d3372
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-19 18:15:46 -0700

    Update reputation.js

--- client/cmds/reputation.js
+++ client/cmds/reputation.js
@@ -82,7 +82,7 @@ module.exports.requirements = {
     clientPerms: ['EMBED_LINKS', 'SEND_MESSAGES'],
     ownerOnly: false,
     betaMode: false,
-    devLock: false
+    devLock: true
 }
 
 module.exports.limits = {
