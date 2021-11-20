71221990938312b7104bddf1c81728c441a932f1
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-19 22:54:20 -0700

    Update index.js

--- client/utils/embeds/index.js
+++ client/utils/embeds/index.js
@@ -1,4 +1,2 @@
-module.exports = async () => {
-   let Version = '2.1.2'
-   let Footer = `© 2021 - 2022 Tox Mod | v2.1.2`
-}
+exports.Version = '2.1.2'
+exports.Footer = `© 2021 - 2022 Tox Mod | v2.1.2`
