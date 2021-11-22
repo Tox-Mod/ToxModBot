05d64e5fb98837a607f01e9da828586760a22ac7
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-22 16:22:02 -0700

    Update toxmod.js

--- models/client/toxmod.js
+++ models/client/toxmod.js
@@ -1,1 +1,18 @@
+const mongoose = require('mongoose');
 
+const ClientSchema = mongoose.Schema({
+  news: {
+    type: String,
+    required: true,
+  },
+  tag:{
+    type: String,
+    required: true,
+  },    
+ time:{
+  type: String,
+  required: true,
+ }
+});
+
+module.exports = mongoose.model('ToxModNews', ClientSchema);
