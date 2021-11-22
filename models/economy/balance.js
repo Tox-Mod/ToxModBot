328fbe1e3b35e2ea5a0db063f822ba122327d4a3
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-22 16:23:38 -0700

    Update balance.js

--- models/economy/balance.js
+++ models/economy/balance.js
@@ -1,1 +1,8 @@
+const mongoose = require("mongoose")
 
+let balance = mongoose.Schema({
+  user: { type: String },
+  balance: { type: Number, default: 0 }
+})
+
+module.exports = mongoose.model("balance", balance)
