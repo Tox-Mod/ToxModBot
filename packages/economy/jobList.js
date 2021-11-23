e8eadb88440be948bac90db48b8fa13c782b0e1d
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-22 19:17:35 -0700

    Create jobList.js

---
+++ packages/economy/jobList.js
@@ -0,0 +1,27 @@
+
+module.exports = [
+{
+   name: "Developer - Frontend",
+   pay: '$1000/week',
+   description: "Work as a Frontend Developer for Microsoft",
+   id: "dev1",
+   type: "Job",
+   company: "Microsoft"
+},
+{
+   name: "Developer - Backend",
+   pay: '$1000/week',
+   description: "Work as a Backend Developer for Microsoft",
+   id: "dev2",
+   type: "Job",
+   company: "Microsoft"
+},
+{
+   name: "Graphics Designer",
+   pay: '$1100/week',
+   description: "Work as a Graphics Designer for Microsoft",
+   id: "gfx",
+   type: "Job",
+   company: "Microsoft"
+ },
+]
