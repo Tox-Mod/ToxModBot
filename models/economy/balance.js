3cdc944f0f3b2c203f10846df9e2a1a5faab3ecb
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-22 19:28:43 -0700

    Update balance.js

--- models/economy/balance.js
+++ models/economy/balance.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 let balance = mongoose.Schema({
   user: { type: String },
-  balance: { type: Number, default: 0 }
+  balance: { type: Number, default: 0 },
+  bank: { type: Number, default: 0}
 })
 
 module.exports = mongoose.model("balance", balance)
