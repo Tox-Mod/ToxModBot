e5f86613ba6256f39e46bdca8513d8fa268b4683
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-11-22 19:30:18 -0700

    Update balance.js

--- client/cmds/balance.js
+++ client/cmds/balance.js
@@ -19,20 +19,24 @@ module.exports.run = async (client, message, args, params) => {
     
     let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member
 
-    let amountOfMoney
+    let amountOfMoney;
+    let bankAccount;
 
 
     await bal.findOne({ user: user.user.id }, async (err, db) => {
       if(!db) {
         new bal({
           user: user.id,
-          balance: 0
+          balance: 0,
+          bank: 0
         })
         .save()
         .catch(err => { return message.channel.send(`I have encountered an error -> ${err}`)})
         amountOfMoney = 0
+        bankAccount = 0
       } else {
         amountOfMoney = db.balance
+        bankAccount = db.bank
       }
 
       const successEmbed = new MessageEmbed()
@@ -40,7 +44,7 @@ module.exports.run = async (client, message, args, params) => {
        .setColor(Colors.Success)
        .setTitle(`${user.user.username}s Balance`)
        .addField('💵 Balance', `$` + `${amountOfMoney}`, true)
-       .addField('💰 Bank', `$` + `0`, true)
+       .addField('💰 Bank', `$` + `${bankAccount}`, true)
        .setTimestamp()
        .setFooter(Embeds.Footer, Images.Animated)
 
