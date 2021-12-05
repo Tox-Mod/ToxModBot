38e2fdae246adc6b5d523729995040971a43d291
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2021-12-04 18:52:56 -0700

    Create hasVoted.js

---
+++ client/cmds/hasVoted.js
@@ -0,0 +1,52 @@
+const { MessageEmbed } = require('discord.js');
+const SERVERS = require('@Database/servers');
+const moment = require('moment');
+const fetch = require('node-fetch');
+const package = require('../../package.json')
+
+const Images = require('@Images/index');
+const Colors = require('@Colors/index');
+const Embeds = require('@Embeds/index');
+const { InfinityBots } = require('infinity-bots');
+
+module.exports.run = async (client, message, args, params) => {
+
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+  
+    if (!user) return message.channel.send("Please provide a user to check if the have voted");
+  
+    await InfinityBots.checkUserVoted(`${user.id}`, '631558023109804032', function(data) => {
+
+    let embed = new MessageEmbed()
+      .setTitle('Info and Statistics')
+      .setColor(Colors.Primary)
+      .setThumbnail(Images.Animated)
+      .setImage(Images.Banner)
+      .setDescription(`${data.hasVoted}`)
+      .setTimestamp()
+      .setFooter(Embeds.Footer, Images.Animated)
+
+      return message.channel.send(embed)
+  })
+}
+
+module.exports.help = {
+    name: 'has-voted',
+    category: 'owner',
+    aliases: ['cv', 'check-voted'],
+    description: 'Check if a User has Voted for Tox Mod on IBL',
+    example: '``has-voted``'
+}
+
+module.exports.requirements = {
+    userPerms: [],
+    clientPerms: ['SEND_MESSAGES', 'EMBED_LINKS'],
+    ownerOnly: true,
+    betaMode: false,
+    devLock: false
+}
+
+module.exports.limits = {
+    rateLimit: 2,
+    cooldown: 1e4
+}
